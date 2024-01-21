eaaab9950ffa462452c6b1d31260b9603aaf11bc
Joel Martin <64128660+joel-mrtn@users.noreply.github.com>
2024-01-21 19:46:20 +0100

    Add center cursor remap to page navigation and search

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -10,6 +10,13 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>pf', builtin.find_files)
 vim.keymap.set('n', '<leader>pg', builtin.git_files)
 
+-- center cursor while navigating
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- center cursor while searching
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
 -- yank to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
