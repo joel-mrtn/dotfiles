afdf5f9eb5f11da2da84487cbc19a1ae382fc4ce
Joel Martin <64128660+joel-mrtn@users.noreply.github.com>
2024-02-08 22:47:43 +0100

    Add iterm shell integration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,6 @@ fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/.cargo/env
 
 set -U fish_greeting
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 starship init fish | source
